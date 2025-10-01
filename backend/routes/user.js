24c3ceadb79fd2fe1093614c61e4bd3453e78310
kietn20 <kietnguyen3698@gmail.com>
2025-10-01 15:08:07 -0700

    install deps

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/user.model");
 
