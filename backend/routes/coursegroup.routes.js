b569f3faa95a76754e526f34d502b632b7ed71fb
Rand0mP3rson <159761817+Rand0mP3rson@users.noreply.github.com>
2025-10-09 15:28:12 -0700

    Update coursegroup.routes.js

--- backend/routes/coursegroup.routes.js
+++ backend/routes/coursegroup.routes.js
@@ -1,7 +1,7 @@
 // Purpose: Defines routes for all course group related actions
 
 const express = require('express');
-const { generateInvite, joinGroup, createCourseGroup } = require('../controllers/coursegroup.controller'); 
+const courseGroupController = require('../controllers/coursegroup.controller'); 
 const { protect } = require('../middleware/auth.middleware');
 
 const router = express.Router();
@@ -10,13 +10,13 @@ const router = express.Router();
 router.use(protect);
 
 //route to create a group
-router.post("/", createCourseGroup);
+router.post("/", courseGroupController.createCourseGroup);
 
 // route to join a group
-router.post('/join', joinGroup);
+router.post('/join', courseGroupController.joinGroup);
 
 // route to generate an invite code for a specific group
-router.get('/:groupId/invite', generateInvite);
+router.get('/:groupId/invite', courseGroupController.generateInvite);
 
 
 module.exports = router;
