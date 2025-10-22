7d1c460fb58d0dcadd930ae757a0be273eed2595
brandon-smsn <151482300+brandon-smsn@users.noreply.github.com>
2025-10-22 15:51:27 -0700

    Implemented members list and redirect non-owners to 403 if accessing group edit page

--- backend/controllers/coursegroup.controller.js
+++ backend/controllers/coursegroup.controller.js
@@ -250,42 +250,6 @@ const leaveGroup = async (req, res) => {
   }
 };
 
-
-// gets all course groups
-const getCourseGroups = (req, res) => {
-  res.status(200).json({ message: "Stub: getCourseGroups" });
-};
-
-// join group via invite
-const addUserToCourseGroup = (req, res) => {
-  res.status(200).json({ message: "Stub: addUserToCourseGroup" });
-};
-
-// leave group (non-owners only)
-const removeUserFromCourseGroup = (req, res) => {
-  res.status(200).json({ message: "Stub: removeUserFromCourseGroup" });
-};
-
-// transfers ownership of a course group to another user
-const transferCourseGroupOwnership = (req, res) => {
-  res.status(200).json({ message: "Stub: transferCourseGroupOwnership" });
-};
-
-// lists all members of a course group
-const getGroupMembers = (req, res) => {
-  res.status(200).json({ message: "Stub: getGroupMembers" });
-};
-
-// fetch group invite link
-const getGroupInviteLink = (req, res) => {
-  res.status(200).json({ message: "Stub: getGroupInviteLink" });
-};
-
-// validate invite before joining
-const validateInviteCode = (req, res) => {
-  res.status(200).json({ message: "Stub: validateInviteCode" });
-};
-
 // @desc    Get all groups where the user is a member
 // @route   GET /api/groups
 // @access  Private
@@ -310,6 +274,49 @@ const getUserGroups = async (req, res) => {
   }
 };
 
+// transfers ownership of a course group to another user
+const transferCourseGroupOwnership = (req, res) => {
+  res.status(200).json({ message: "Stub: transferCourseGroupOwnership" });
+};
+
+
+// gets all course groups
+// not really necessary we have getUserGroups
+const getCourseGroups = (req, res) => {
+  res.status(200).json({ message: "Stub: getCourseGroups" });
+};
+
+// join group via invite
+// we already have joinGroup
+const addUserToCourseGroup = (req, res) => {
+  res.status(200).json({ message: "Stub: addUserToCourseGroup" });
+};
+
+// leave group (non-owners only)
+// leaveGroup function along with middleware handles this already
+const removeUserFromCourseGroup = (req, res) => {
+  res.status(200).json({ message: "Stub: removeUserFromCourseGroup" });
+};
+
+
+// lists all members of a course group
+// we do not need this for displaying members
+// but we could use it later if we need only members w/o group data
+const getGroupMembers = (req, res) => {
+  res.status(200).json({ message: "Stub: getGroupMembers" });
+};
+
+// fetch group invite link
+// we dont need this we already have generateInvite
+const getGroupInviteLink = (req, res) => {
+  res.status(200).json({ message: "Stub: getGroupInviteLink" });
+};
+
+// validate invite before joining
+// we dont need this, join group already validates it
+const validateInviteCode = (req, res) => {
+  res.status(200).json({ message: "Stub: validateInviteCode" });
+};
 
 module.exports = {
   generateInvite,
