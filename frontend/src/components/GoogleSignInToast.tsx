bd5d031ee8073ef20d0b193cfa2beafa9fab6639
brandon-smsn <151482300+brandon-smsn@users.noreply.github.com>
2025-12-02 13:30:40 -0800

    Removed console log

--- frontend/src/components/GoogleSignInToast.tsx
+++ frontend/src/components/GoogleSignInToast.tsx
@@ -7,7 +7,6 @@ export default function GoogleSignInToast() {
   const { user } = useAuth();
 
   useEffect(() => {
-    console.log("GoogleSignInToast effect:", user, localStorage.getItem("googleSignIn"));
     if (user && localStorage.getItem("googleSignIn") === "true") {
       toast.success("Logged in successfully.");
       localStorage.removeItem("googleSignIn");
